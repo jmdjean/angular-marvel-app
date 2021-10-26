5827c3ca6dcf6912966db1589cb0916c6de9b8ea
jmdjean <jean.medes@totvs.com.br>
2021-10-25 23:43:44 -0300

    ajuste footer

--- src/app/core/components/footer/footer.component.ts
+++ src/app/core/components/footer/footer.component.ts
@@ -7,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FooterComponent implements OnInit {
 
-  year: number = new Date().getFullYear();
+  currentYear: number = new Date().getFullYear();
 
   constructor() { }
 
